e543019f78eeb3dbe45aff05e3aef28c27e8e98d
Vicente Feliu <vicentemariafeliu@gmail.com>
2024-08-14 12:03:38 +0200

    fixing name of the project

--- first_bash_example.sh
+++ first_bash_example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /Users/vicentefeliu/PersonalProjects/PlaywrightPadelProject    # Navigate to your project directory
-npx playwright test --headed
+npx playwright test --headed # Run the program
